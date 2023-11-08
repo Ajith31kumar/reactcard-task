ec93aaffe93ed28ead992f538ea6d72de4890d37
Ajith31kumar <ajimccullum06952@gmail.com>
2023-11-08 23:54:04 +0530

    Message

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-//import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
 function App() {
+  const plan = [
+    {
+      plan: "Free",
+      price: "$0",
+      users: "Single User",
+      projects: "5GB Storage",
+      access: "Community Access",
+      privateproj: "Unlimited Private Projects",
+      phone: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      reports: "Monthly Status Reports"
+    },
+    {
+      plan: "Plus",
+      price: "$9",
+      users: "Single User",
+      projects: "50GB Storage",
+      access: "Community Access",
+      privateproj: "Unlimited Private Projects",
+      phone: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      reports: "Monthly Status Reports"
+    },
+    {
+      plan: "Pro",
+      price: "$49",
+      users: "Unlimited users",
+      projects: "150GB Storage",
+      access: "Community Access",
+      privateproj: "Unlimited Private Projects",
+      phone: "Dedicated Phone Support",
+      subdomain: "Unlimited Free Subdomain",
+      reports: "Monthly Status Reports"
+    }
+  ];
+
   return (
-    <section className="pricing py-5">
-      <div className="container">
-        <div className="row">
-          <Message
-            plan="Free"
-            price="$0"
-            users="Single User"
-            projects="5GB Storage"
-            access="Community Access"
-            privateproj="Unlimited Private Projects"
-            phone="Dedicated Phone Support"
-            subdomain="Free Subdomain"
-            reports="Monthly Status Reports"></Message>
-          
-          <Message
-            plan="Pluse"
-            price="$9"
-            users="Single User"
-            projects="50GB Storage"
-            access="Community Access"
-            privateproj="Unlimited Private Projects"
-            phone="Dedicated Phone Support"
-            subdomain="Free Subdomain"
-            reports="Monthly Status Reports"></Message>
-          
-          <Message
-            plan="Pro"
-            price="$49"
-            users="Unlimited users"
-            projects="150GB Storage"
-            access="Community Access"
-            privateproj="Unlimited Private Projects"
-            phone="Dedicated Phone Support"
-            subdomain="Unlimited Free Subdomain"
-            reports="Monthly Status Reports"></Message>
-         {/* <div></div> */}
+    <section className='pricing py-5'>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='text-center'>PRICE CARD</h1>
+          {plan.map((detail, index) => (
+            <Message key={index} details={detail} />
+          ))}
         </div>
       </div>
     </section>
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-function Message({ plan, price, users, projects, access, privateproj, phone, subdomain, reports }) {
+function Message({ details }) {
   return (
     <div className="col-lg-4">
       <div className="card mb-5 mb-lg-0">
         <div className="card-body">
-          <h5 className="card-title text-muted text-uppercase text-center">{plan}</h5>
-          <h6 className="card-price text-center">{price}<span className="period">/month</span></h6>
+          <h5 className="card-title text-muted text-uppercase text-center">{details.plan}</h5>
+          <h6 className="card-price text-center">{details.price}<span className="period">/month</span></h6>
           <ul className="fa-ul">
-            <li><span className="fa-li"><i className="fas fa-check"></i></span>{users}</li>
-            <li><span className="fa-li"><i className="fas fa-check"></i></span>{projects}</li>
-            <li><span className="fa-li"><i className="fas fa-check"></i></span>{access}</li>
-            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{privateproj}</li>
-            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{phone}</li>
-            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{subdomain}</li>
-            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{reports}</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.users}</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.projects}</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.access}</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.privateproj}</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.phone}</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.subdomain}</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.reports}</li>
           </ul>
           <div className="d-grid">
             <a href="#" className="btn btn-primary text-uppercase">Button</a>
